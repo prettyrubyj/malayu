f3aaecfb3393e5ba6afee65651d528347a1c3a3c
prettyrubyj <prettyrubyj@gmail.com>
2018-03-02 09:33:15 +0800

    change back to default data

--- assets/js/categoryRevenue.js
+++ assets/js/categoryRevenue.js
@@ -11,15 +11,57 @@ $.ajax({
 			var result = rs;
       allData=result[0].data;
 			console.log(result[0].data);
-      drawPieChart(allData);
+      //drawPieChart(allData);
 		},
 		error: function(){
-			alert('系統錯誤，請重新整理');
+			//alert('系統錯誤，請重新整理');
 		}
 	});
-
-  function drawPieChart(allData){
-    console.log(allData);
+  
+alldata=[
+{
+name: "P1",
+y: 1075
+},
+{
+name: "VP",
+y: 1048
+},
+{
+name: "RW",
+y: 1039
+},
+{
+name: "PSN",
+y: 708
+},
+{
+name: "TRA",
+y: 591
+},
+{
+name: "VC",
+y: 330
+},
+{
+name: "SAM",
+y: 289
+},
+{
+name: "PLP",
+y: 196
+},
+{
+name: "PDV",
+y: 190
+},
+{
+name: "ADS",
+y: 185
+}
+];
+// console.log(alldata);
+  //function drawPieChart(allData){
     Highcharts.chart('categoryRevenue', {
         //colors: ['#FAA05A', '#2FA2F4','#F7652B'],
         chart: {
@@ -47,7 +89,7 @@ $.ajax({
         series: [{
               name: 'Brands',
               colorByPoint: true,
-              data: allData
+              data: alldata
               // data: [{
               //     name: 'Microsoft Internet Explorer',
               //     y: 56.33
@@ -62,4 +104,4 @@ $.ajax({
               // }]
           }]
       });
-  }
+  //}
