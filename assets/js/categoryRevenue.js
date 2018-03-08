6d95dde630ea83b99b6f07248bace4f84dbb0a90
prettyrubyj <prettyrubyj@gmail.com>
2018-03-09 04:02:08 +0800

    update overview linecharts

--- assets/js/categoryRevenue.js
+++ assets/js/categoryRevenue.js
@@ -1,7 +1,7 @@
 // Build the chart
 var alldata;
 $.ajax({
-	    url: 'http://api.awsfox.com:32885/trans',
+	    url: 'http://api.awsfox.com/trans',
 	    global: false,
 	    cache: false,
 	    type: "get",
@@ -10,58 +10,58 @@ $.ajax({
 		success: function(rs) {
 			var result = rs;
       allData=result[0].data;
-			console.log(result[0].data);
-      //drawPieChart(allData);
+			//console.log(result[0].data);
+      drawPieChart(allData);
 		},
 		error: function(){
 			//alert('系統錯誤，請重新整理');
 		}
 	});
-  
-alldata=[
-{
-name: "P1",
-y: 1075
-},
-{
-name: "VP",
-y: 1048
-},
-{
-name: "RW",
-y: 1039
-},
-{
-name: "PSN",
-y: 708
-},
-{
-name: "TRA",
-y: 591
-},
-{
-name: "VC",
-y: 330
-},
-{
-name: "SAM",
-y: 289
-},
-{
-name: "PLP",
-y: 196
-},
-{
-name: "PDV",
-y: 190
-},
-{
-name: "ADS",
-y: 185
-}
-];
+
+// alldata=[
+// {
+// name: "P1",
+// y: 1075
+// },
+// {
+// name: "VP",
+// y: 1048
+// },
+// {
+// name: "RW",
+// y: 1039
+// },
+// {
+// name: "PSN",
+// y: 708
+// },
+// {
+// name: "TRA",
+// y: 591
+// },
+// {
+// name: "VC",
+// y: 330
+// },
+// {
+// name: "SAM",
+// y: 289
+// },
+// {
+// name: "PLP",
+// y: 196
+// },
+// {
+// name: "PDV",
+// y: 190
+// },
+// {
+// name: "ADS",
+// y: 185
+// }
+// ];
 // console.log(alldata);
-  //function drawPieChart(allData){
+  function drawPieChart(allData){
     Highcharts.chart('categoryRevenue', {
         //colors: ['#FAA05A', '#2FA2F4','#F7652B'],
         chart: {
@@ -89,7 +89,7 @@ y: 185
         series: [{
               name: 'Brands',
               colorByPoint: true,
-              data: alldata
+              data: allData
               // data: [{
               //     name: 'Microsoft Internet Explorer',
               //     y: 56.33
@@ -104,4 +104,4 @@ y: 185
               // }]
           }]
       });
-  //}
+  }
---
+++ assets/js/overviewLineChart.js
@@ -0,0 +1,467 @@
+// Build the chart
+var trafficData;
+var trafficDate;
+
+//取traffic 資料
+$.ajax({
+	    url: 'http://api.awsfox.com/dailySales/7',
+	    global: false,
+	    cache: false,
+	    type: "get",
+	    contentType: 'application/json',
+		  dataType: "json",
+		success: function(rs) {
+			var result = rs;
+      trafficDate=result[0].date;
+      trafficData=result[0].data;
+      document.getElementById('trafficCount').innerHTML=result[0].salesAmount;
+      //轉換百分比為正負值，用百分比減100可得當日比昨日上升或減少，取到小數第二位
+      percentOri=result[0].percentage.split("%",1);
+      percentNew=((percentOri-100).toFixed(2)).toString()+"%";
+      document.getElementById('trafficPersent').innerHTML=percentNew;
+      drawTraffic(trafficDate,trafficData);
+		},
+		error: function(){
+			//alert('系統錯誤，請重新整理');
+		}
+	});
+
+//取sales 資料
+  $.ajax({
+  	    url: 'http://api.awsfox.com/dailyCustomer/7',
+  	    global: false,
+  	    cache: false,
+  	    type: "get",
+  	    contentType: 'application/json',
+  		  dataType: "json",
+  		success: function(rs) {
+  			var result = rs;
+        salesDate=result[0].date;
+        salesData=result[0].data;
+        document.getElementById('salesCount').innerHTML=result[0].customerAmount;
+        //轉換百分比為正負值，用百分比減100可得當日比昨日上升或減少，取到小數第二位
+        percentOri=result[0].percentage.split("%",1);
+        percentNew=((percentOri-100).toFixed(2)).toString()+"%";
+        console.log(percentNew);
+        document.getElementById('salesPersent').innerHTML=percentNew;
+        drawSales(salesDate,salesData);
+  		},
+  		error: function(){
+  			//alert('系統錯誤，請重新整理');
+  		}
+  	});
+
+//WidgetChart 1
+function drawTraffic(trafficDate,trafficData){
+  var ctx = document.getElementById( "traffic_wg" );
+  ctx.height = 150;
+  var myChart = new Chart( ctx, {
+      type: 'line',
+      data: {
+          //labels: ['2/23', '2/24', '2/25', '2/26', '2/27', '2/28', '3/1'],
+          labels: trafficDate,
+          type: 'line',
+          datasets: [ {
+              //data: [65, 59, 84, 84, 51, 55, 40],
+              data:trafficData,
+              label: 'Dataset',
+              backgroundColor: 'transparent',
+              borderColor: 'rgba(255,255,255,.55)',
+          }, ]
+      },
+      options: {
+
+          maintainAspectRatio: false,
+          legend: {
+              display: false
+          },
+          responsive: true,
+          tooltips: {
+              mode: 'index',
+              titleFontSize: 12,
+              titleFontColor: '#000',
+              bodyFontColor: '#000',
+              backgroundColor: '#fff',
+              titleFontFamily: 'Montserrat',
+              bodyFontFamily: 'Montserrat',
+              cornerRadius: 3,
+              intersect: false,
+          },
+          scales: {
+              xAxes: [ {
+                  gridLines: {
+                      color: 'transparent',
+                      zeroLineColor: 'transparent'
+                  },
+                  ticks: {
+                      fontSize: 2,
+                      fontColor: 'transparent'
+                  }
+              } ],
+              yAxes: [ {
+                  display:false,
+                  ticks: {
+                      display: false,
+                  }
+              } ]
+          },
+          title: {
+              display: false,
+          },
+          elements: {
+              line: {
+                  borderWidth: 1
+              },
+              point: {
+                  radius: 4,
+                  hitRadius: 10,
+                  hoverRadius: 4
+              }
+          }
+      }
+  } );
+}
+
+
+
+//WidgetChart 2
+function drawSales(salesDate,salesData){
+  var ctx = document.getElementById( "sales_wg" );
+  ctx.height = 150;
+  var myChart = new Chart( ctx, {
+      type: 'line',
+      data: {
+          //labels: ['2/23', '2/24', '2/25', '2/26', '2/27', '2/28', '3/1'],
+          labels:salesDate,
+          type: 'line',
+          datasets: [ {
+              //data: [1, 18, 9, 17, 34, 22, 11],
+              data:salesData,
+              label: 'Dataset',
+              backgroundColor: '#63c2de',
+              borderColor: 'rgba(255,255,255,.55)',
+          }, ]
+      },
+      options: {
+
+          maintainAspectRatio: false,
+          legend: {
+              display: false
+          },
+          responsive: true,
+          tooltips: {
+              mode: 'index',
+              titleFontSize: 12,
+              titleFontColor: '#000',
+              bodyFontColor: '#000',
+              backgroundColor: '#fff',
+              titleFontFamily: 'Montserrat',
+              bodyFontFamily: 'Montserrat',
+              cornerRadius: 3,
+              intersect: false,
+          },
+          scales: {
+              xAxes: [ {
+                  gridLines: {
+                      color: 'transparent',
+                      zeroLineColor: 'transparent'
+                  },
+                  ticks: {
+                      fontSize: 2,
+                      fontColor: 'transparent'
+                  }
+              } ],
+              yAxes: [ {
+                  display:false,
+                  ticks: {
+                      display: false,
+                  }
+              } ]
+          },
+          title: {
+              display: false,
+          },
+          elements: {
+              line: {
+                  tension: 0.00001,
+                  borderWidth: 1
+              },
+              point: {
+                  radius: 4,
+                  hitRadius: 10,
+                  hoverRadius: 4
+              }
+          }
+      }
+  } );
+}
+
+
+
+//WidgetChart 3
+var ctx = document.getElementById( "conversion_wg" );
+ctx.height = 150;
+var myChart = new Chart( ctx, {
+    type: 'line',
+    data: {
+        labels: ['2/23', '2/24', '2/25', '2/26', '2/27', '2/28', '3/1'],
+        type: 'line',
+        datasets: [ {
+            data: [65, 59, 84, 84, 51, 55, 40],
+            label: 'Dataset',
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(255,255,255,.55)',
+        }, ]
+    },
+    options: {
+
+        maintainAspectRatio: false,
+        legend: {
+            display: false
+        },
+        responsive: true,
+        tooltips: {
+            mode: 'index',
+            titleFontSize: 12,
+            titleFontColor: '#000',
+            bodyFontColor: '#000',
+            backgroundColor: '#fff',
+            titleFontFamily: 'Montserrat',
+            bodyFontFamily: 'Montserrat',
+            cornerRadius: 3,
+            intersect: false,
+        },
+        scales: {
+            xAxes: [ {
+                gridLines: {
+                    color: 'transparent',
+                    zeroLineColor: 'transparent'
+                },
+                ticks: {
+                    fontSize: 2,
+                    fontColor: 'transparent'
+                }
+            } ],
+            yAxes: [ {
+                display:false,
+                ticks: {
+                    display: false,
+                }
+            } ]
+        },
+        title: {
+            display: false,
+        },
+        elements: {
+            line: {
+                borderWidth: 1
+            },
+            point: {
+                radius: 4,
+                hitRadius: 10,
+                hoverRadius: 4
+            }
+        }
+    }
+} );
+
+//WidgetChart 4
+var ctx = document.getElementById( "shopperYield_wg" );
+ctx.height = 150;
+var myChart = new Chart( ctx, {
+    type: 'line',
+    data: {
+        labels: ['2/23', '2/24', '2/25', '2/26', '2/27', '2/28', '3/1'],
+        type: 'line',
+        datasets: [ {
+            data: [65, 59, 84, 84, 51, 55, 40],
+            label: 'Dataset',
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(255,255,255,.55)',
+        }, ]
+    },
+    options: {
+
+        maintainAspectRatio: false,
+        legend: {
+            display: false
+        },
+        responsive: true,
+        tooltips: {
+            mode: 'index',
+            titleFontSize: 12,
+            titleFontColor: '#000',
+            bodyFontColor: '#000',
+            backgroundColor: '#fff',
+            titleFontFamily: 'Montserrat',
+            bodyFontFamily: 'Montserrat',
+            cornerRadius: 3,
+            intersect: false,
+        },
+        scales: {
+            xAxes: [ {
+                gridLines: {
+                    color: 'transparent',
+                    zeroLineColor: 'transparent'
+                },
+                ticks: {
+                    fontSize: 2,
+                    fontColor: 'transparent'
+                }
+            } ],
+            yAxes: [ {
+                display:false,
+                ticks: {
+                    display: false,
+                }
+            } ]
+        },
+        title: {
+            display: false,
+        },
+        elements: {
+            line: {
+                borderWidth: 1
+            },
+            point: {
+                radius: 4,
+                hitRadius: 10,
+                hoverRadius: 4
+            }
+        }
+    }
+} );
+
+//WidgetChart 5
+var ctx = document.getElementById( "captureRate_wg" );
+ctx.height = 150;
+var myChart = new Chart( ctx, {
+    type: 'line',
+    data: {
+        labels: ['2/23', '2/24', '2/25', '2/26', '2/27', '2/28', '3/1'],
+        type: 'line',
+        datasets: [ {
+            data: [65, 59, 84, 84, 51, 55, 40],
+            label: 'Dataset',
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(255,255,255,.55)',
+        }, ]
+    },
+    options: {
+
+        maintainAspectRatio: false,
+        legend: {
+            display: false
+        },
+        responsive: true,
+        tooltips: {
+            mode: 'index',
+            titleFontSize: 12,
+            titleFontColor: '#000',
+            bodyFontColor: '#000',
+            backgroundColor: '#fff',
+            titleFontFamily: 'Montserrat',
+            bodyFontFamily: 'Montserrat',
+            cornerRadius: 3,
+            intersect: false,
+        },
+        scales: {
+            xAxes: [ {
+                gridLines: {
+                    color: 'transparent',
+                    zeroLineColor: 'transparent'
+                },
+                ticks: {
+                    fontSize: 2,
+                    fontColor: 'transparent'
+                }
+            } ],
+            yAxes: [ {
+                display:false,
+                ticks: {
+                    display: false,
+                }
+            } ]
+        },
+        title: {
+            display: false,
+        },
+        elements: {
+            line: {
+                borderWidth: 1
+            },
+            point: {
+                radius: 4,
+                hitRadius: 10,
+                hoverRadius: 4
+            }
+        }
+    }
+} );
+
+//WidgetChart 6
+var ctx = document.getElementById( "newShoppers_wg" );
+ctx.height = 150;
+var myChart = new Chart( ctx, {
+    type: 'line',
+    data: {
+        labels: ['2/23', '2/24', '2/25', '2/26', '2/27', '2/28', '3/1'],
+        type: 'line',
+        datasets: [ {
+            data: [65, 59, 84, 84, 51, 55, 40],
+            label: 'Dataset',
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(255,255,255,.55)',
+        }, ]
+    },
+    options: {
+
+        maintainAspectRatio: false,
+        legend: {
+            display: false
+        },
+        responsive: true,
+        tooltips: {
+            mode: 'index',
+            titleFontSize: 12,
+            titleFontColor: '#000',
+            bodyFontColor: '#000',
+            backgroundColor: '#fff',
+            titleFontFamily: 'Montserrat',
+            bodyFontFamily: 'Montserrat',
+            cornerRadius: 3,
+            intersect: false,
+        },
+        scales: {
+            xAxes: [ {
+                gridLines: {
+                    color: 'transparent',
+                    zeroLineColor: 'transparent'
+                },
+                ticks: {
+                    fontSize: 2,
+                    fontColor: 'transparent'
+                }
+            } ],
+            yAxes: [ {
+                display:false,
+                ticks: {
+                    display: false,
+                }
+            } ]
+        },
+        title: {
+            display: false,
+        },
+        elements: {
+            line: {
+                borderWidth: 1
+            },
+            point: {
+                radius: 4,
+                hitRadius: 10,
+                hoverRadius: 4
+            }
+        }
+    }
+} );
